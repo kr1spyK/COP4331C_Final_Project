07464008b9217b51a914dbe28314e757085f7660
davidjmaria <david.j.maria@ieee.org>
2018-10-09 17:57:46 +0000

    api: Add app configuration file
    added configuration file and an example of how to access config variables.

--- api/api.py
+++ api/api.py
@@ -3,6 +3,7 @@ from flask_restful import reqparse, abort, Api, Resource
 from endpoints.test import *
 
 app = Flask(__name__)
+app.config.from_pyfile('api.cfg')
 api = Api(app)
 
 api.add_resource(TestEndpoint, '/test')
--- api/endpoints/test.py
+++ api/endpoints/test.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import current_app as app
 from flask_restful import reqparse, abort, Api, Resource
 
 class TestEndpoint(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'hello': 'world', 'Config_Var': app.config['TEST_VAR']}
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('message')
