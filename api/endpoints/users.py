fad95c4bc26a81b82184bcbd40b7847c67290563
davidjmaria <david.j.maria@ieee.org>
2018-10-18 12:27:06 -0400

    api: Hash passwords before adding to DB

--- api/endpoints/users.py
+++ api/endpoints/users.py
@@ -3,6 +3,7 @@ from flask import current_app as app
 from flask_restful import reqparse, abort, Api, Resource
 from database.db import getSession
 from database.models import *
+from argon2 import PasswordHasher
 
 
 class UserRegisterEndpoint(Resource):
@@ -21,10 +22,13 @@ class UserRegisterEndpoint(Resource):
         if session.query(User).filter_by(username=json_data["username"]).first():
             return jsonify({"success": -1,
                             "error": "User already registered"})
+        # hash password
+        ph = PasswordHasher()
+        hash = ph.hash(json_data["password"])
 
         try:
             new_user = User(username=json_data["username"],
-                            password=json_data["password"],
+                            password=hash,
                             admin=json_data["isAdmin"])
 
             session.add(new_user)
