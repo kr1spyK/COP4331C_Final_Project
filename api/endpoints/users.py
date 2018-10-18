bc2dc077a8bb00297d6ac7a1509291e7bf77b082
davidjmaria <david.j.maria@ieee.org>
2018-10-18 12:11:37 -0400

    api: Clean up register api

--- api/endpoints/users.py
+++ api/endpoints/users.py
@@ -3,7 +3,7 @@ from flask import current_app as app
 from flask_restful import reqparse, abort, Api, Resource
 from database.db import getSession
 from database.models import *
-import json
+
 
 class UserRegisterEndpoint(Resource):
     def post(self):
@@ -12,23 +12,21 @@ class UserRegisterEndpoint(Resource):
         json_data = request.get_json(force=True)
         for field in required_fields:
             if field not in json_data.keys():
-                return jsonify({"success": -1, 
-                                "error": "Missing {} field".format(field)
-                               })
+                return jsonify({"success": -1,
+                                "error": "Missing {} field".format(field)})
         # create database session
         session = getSession(app.config["DB_USER"], app.config["DB_PASS"])
 
         # check to see if username is taken
         if session.query(User).filter_by(username=json_data["username"]).first():
-           return jsonify({"success": -1, 
-                            "error": "User already registered"
-                           })
+            return jsonify({"success": -1,
+                            "error": "User already registered"})
 
         try:
             new_user = User(username=json_data["username"],
                             password=json_data["password"],
-                            admin=json_data["isAdmin"]
-                           )
+                            admin=json_data["isAdmin"])
+
             session.add(new_user)
             session.commit()
             return jsonify({"success": 1})
