5b4c4909e3037f75de2d2cdf6ecfd1482754b8c8
kr1spyK <toffysk@gmail.com>
2018-10-23 15:42:32 -0400

    Add main activity

--- AndroidApp/app/src/main/java/cf/poosgroup5_u/bugipedia/LoginActivity.java
+++ AndroidApp/app/src/main/java/cf/poosgroup5_u/bugipedia/LoginActivity.java
@@ -161,7 +161,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         // Check for a valid password, if the user entered one.
         if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
+            mPasswordView.setError("Password must be greater than 8 characters and contain 1 number");
             focusView = mPasswordView;
             cancel = true;
         }
@@ -197,7 +197,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
+<<<<<<< HEAD
         return password.length() > 4;
+=======
+        return password.length() > 8;
+>>>>>>> Add main activity
     }
 
     /**
