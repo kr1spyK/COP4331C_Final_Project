6d3d18768558f9678e79c854887a8826c9e84ac1
Airor4 <31018916+Airor4@users.noreply.github.com>
2018-11-06 08:42:28 -0500

    fragment and activity working

---
+++ AndroidApp/app/src/main/java/cf/poosgroup5_u/bugipedia/BuggyMain.java
@@ -0,0 +1,13 @@
+package cf.poosgroup5_u.bugipedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuggyMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searcher_fragment_layout);
+    }
+}
