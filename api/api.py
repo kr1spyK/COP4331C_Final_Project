08722473bba222c7e642df66d0d7639994970f2d
derplime <edolorfino24@gmail.com>
2018-11-10 11:44:30 -0500

    changes to api.py, no testing

--- api/api.py
+++ api/api.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 from endpoints.test import *
-#from endpoints.users import *
+from endpoints.users import *
 from endpoints.bug import *
 from database.db import  getSession
 
@@ -12,8 +12,8 @@ api = Api(app)
 api.add_resource(TestEndpoint, '/test')
 api.add_resource(TestAuthEndpoint, '/testAuth')
 api.add_resource(DbTestEndpoint, '/dbTest')
-#api.add_resource(UserRegisterEndpoint, '/register')
-#api.add_resource(LoginEndpoint, '/login')
+api.add_resource(UserRegisterEndpoint, '/register')
+api.add_resource(LoginEndpoint, '/login')
 api.add_resource(BugRegisterEndpoint, '/registerBug')
 
 
