a74b46d93e18052c5a4da01ed72fca3aa6f6ecfc
Sara <frackie.sara@gmail.com>
2018-11-29 13:21:44 -0500

    update

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -151,18 +151,64 @@ function addBug()
         var mouth = document.getElementById("mouth_parts").value;
         var descrip = document.getElementById("description").value;
         var additional = document.getElementById("additional_advice").value;
-    } catch (e) {
 
+        var wings = 0;
+        var antenna = 0;
+        var hindleg = 0;
+        var furry = 0;
+        var thin = 0;
+        if (document.getElementById("wings").value == "True")
+            wings = 1;
+        if (document.getElementById("antenna").value == "True")
+            antenna = 1;
+        if (document.getElementById("hind_legs").value == "True")
+            hindleg = 1;
+        if (document.getElementById("hairy_furry").value == "True")
+            furry = 1;
+        if (document.getElementById("thin_body").value == "True")
+            thin = 1;
+
+    } catch (e) {
+        $(document.getElementById("addMessage")).append('<div class="alert alert-danger alert-dismissible" id="errorRegister">  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>  <strong>Error!</strong> Could not add bug. </div>');
     }
     
 
     // check if any fields are empty
     if (commonName == "" || scientificName == "" || clss == "" || order == "" || family == "" || genus == "" || color1 == "" || color2 == "" || type == "" || mouth == "" || descrip == "" || additional == "")
     {
-        $(document.getElementById("addMessage")).append('<div class="alert alert-danger alert-dismissible" id="badRegister">  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>  <strong>Missing information!</strong> Make sure all of the fields are filled out to add a bug. </div>');
-
+        $(document.getElementById("addMessage")).append('<div class="alert alert-danger alert-dismissible" id="missingIngo">  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>  <strong>Missing information!</strong> Make sure all of the fields are filled out to add a bug. </div>');
     }
 
+
+    var jsonPayload = '{"common_name":"' + commonName + '", "scientific_name":"' + scientificName + '", "class":"' + clss +
+        '", "order":"' + order + '", "family":"' + family + '", "genus":"' + genus + '", "color_1":"' + color1 +
+        '", "color_2":"' + color2 + '", "general_type":"' + type + '", "mouth_parts":"' + mouth + '", "wings":' + wings +
+        ', "antenna":' + antenna + ', "hind_legs_jump":' + hindleg + ', "hairy_furry":' + furry + ', "thin_body":' + thin +
+        ', "description":"' + descrip + '", "additional_advice":"' + additional + '"}';
+    var url = urlBase + 'api/registerBug';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, false);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try {
+        xhr.send(jsonPayload);
+        var result = JSON.parse(xhr.responseText).success;
+
+        if (result == 1) {
+            // successfully added bug
+            $(document.getElementById("addMessage")).append('<div class="alert alert-success alert-dismissible">  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>  <strong>Success!</strong> Added bug. </div>');
+
+        }
+        else {
+            $(document.getElementById("addMessage")).append('<div class="alert alert-danger alert-dismissible">  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>  <strong>Could not add bug!</strong> Is it already in the database? </div>');
+
+        }
+
+    }
+    catch (err) {
+        alert(err.message)
+    }
 }
 
 //function submitEdit()
